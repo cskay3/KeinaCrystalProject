fb93681595f4b223b49ff6767eae11fae1c6ba1a
kga6829 <kga6829@student.pps.net>
2026-05-19 13:43:04 -0700

    done

--- ToneReader.java
+++ ToneReader.java
@@ -5,12 +5,25 @@ import java.util.ArrayList;
 
 public class ToneReader {
 
+    static Scanner myScanner = new Scanner(System.in);
+    add();
+
     public static ArrayList<String> POS(){
         ArrayList
 
     }
 
-    public static void add() tjrows IOException {
-        File positive = new File()
+
+}
+
+public static void add() throws IOException {
+    File positive = new File("positive.txt");
+    Scanner po = new Scanner(positive);
+
+    ArrayList<String> pos = new ArrayList<>();
+
+    while (po.hasNext()){
+        pos.add(po.nextLine());
     }
+
 }
