8393ffd35c93f95607e91d5af0e21c1fd3ea869b
kga6829 <kga6829@student.pps.net>
2026-05-19 13:18:38 -0700

    done

--- ToneReader.java
+++ ToneReader.java
@@ -4,5 +4,7 @@ public class ToneReader {
     public static Arraylist<String> POS(){
         //nouns pronouns verbs adjectives adverbs preposition conjuctions interjections numeral article determiner
         ArrayList<String> article = ["a", "an", "the"];
+        ArrayList<String> nouns;
+
     }
 }
