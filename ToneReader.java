b1391e35405966a32d2cd2872fd242eba41a9c38
cli0618 <Cli0618@student.pps.net>
2026-05-21 13:12:05 -0700

    a

--- ToneReader.java
+++ ToneReader.java
@@ -9,23 +9,27 @@ public class ToneReader {
     System.out.println("Type your comment here:");
     String userInput = myScanner.next)().toLowerCase();
 
+    public static void add() throws IOException {
+        File positive = new File("positive.txt");
+        Scanner po = new Scanner(positive);
 
-    public static String posOrNeg(){
+        ArrayList<String> pos = new ArrayList<>();
 
+        while (po.hasNext()){
+            pos.add(po.nextLine());
+        }
+
+    }
+
+    public static boolean posOrNeg(){
+        boolean positive = false
+
+        if (userInput.indexOf(pos) == -1){
+            return positive;
+        }
+
+        if
     }
 
     public
-
-
-
-public static void add() throws IOException {
-    File positive = new File("positive.txt");
-    Scanner po = new Scanner(positive);
-
-    ArrayList<String> pos = new ArrayList<>();
-
-    while (po.hasNext()){
-        pos.add(po.nextLine());
-    }
-
 }
