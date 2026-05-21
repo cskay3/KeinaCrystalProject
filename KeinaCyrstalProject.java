a635081d34b92747f0e00fa56dc952c0a05d860e
cli0618 <Cli0618@student.pps.net>
2026-05-21 14:05:48 -0700

    a

--- KeinaCyrstalProject.java
+++ KeinaCyrstalProject.java
@@ -36,16 +36,20 @@ public class KeinaCyrstalProject {
         for (String word : pos) {
             if (userInput.indexOf(word) > -1) {
                 System.out.println("positive");
+                break;
             }
-            break;
         }
+
         for (String word : neg) {
             if (userInput.indexOf(word) > -1) {
                 System.out.println("negative");
+                break;
             }
-            break;
+            else {
+                System.out.println("neutral");
+                break;
+            }
         }
-        System.out.println("neutral");
     }
 }
 
