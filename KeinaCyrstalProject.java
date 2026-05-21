93fe3dac110028bbac698e63f346404aecf43555
kga6829 <kga6829@student.pps.net>
2026-05-21 14:04:45 -0700

    done

--- KeinaCyrstalProject.java
+++ KeinaCyrstalProject.java
@@ -36,16 +36,16 @@ public class KeinaCyrstalProject {
         for (String word : pos) {
             if (userInput.indexOf(word) > -1) {
                 System.out.println("positive");
+                break;
             }
-            break;
         }
         for (String word : neg) {
             if (userInput.indexOf(word) > -1) {
                 System.out.println("negative");
+                break;
             }
-            break;
         }
-        System.out.println("neutral");
+
     }
 }
 
