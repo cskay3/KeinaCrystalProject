310a766cf905320901150d36dbc6cd90ace2cf89
kga6829 <kga6829@student.pps.net>
2026-05-28 13:42:17 -0700

    done

--- KeinaCyrstalProject.java
+++ KeinaCyrstalProject.java
@@ -13,6 +13,14 @@ public class KeinaCyrstalProject {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Type your comment here: ");
         String userInput = myScanner.nextLine().toLowerCase();
+        ArrayList<String> sentences = new ArrayList<String>();
+        sentences.add(userInput.split(". "));
+        int i = 0;
+        String[] word = new String[];
+        while (i < sentences.length){
+            sentence = sentences[i]
+        }
+
 
         File positive = new File("positive.txt");
         Scanner po = new Scanner(positive);
@@ -34,18 +42,19 @@ public class KeinaCyrstalProject {
 
         int counterpos = 0;
         int counterneg = 0;
+        int i = 0;
 
+        for (word : sentence){
+                if (userInput.indexOf(word) > -1) {
+                    System.out.println("positive");
+                    break;
+                }
+                else{
+                    counterpos++;
+                }
+                i++;
 
-        for (String word : pos) {
-            if (userInput.indexOf(word) > -1) {
-                System.out.println("positive");
-                break;
             }
-            else{
-                counterpos++;
-            }
-
-        }
         if (counterpos == pos.size()) {
             for (String word1 : neg) {
                 if (userInput.indexOf(word1) > -1) {
