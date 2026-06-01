9485e3e2d40ba4cf3cef5ca55f9c277877460af0
kga6829 <kga6829@student.pps.net>
2026-06-01 13:21:22 -0700

    done

--- KeinaCyrstalProject.java
+++ KeinaCyrstalProject.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class KeinaCyrstalProject {
 
     public static void main(String[] args) throws IOException {
-        posOrNeg();
+        //posOrNeg();
     }
 
     public static void posOrNeg() throws IOException {
@@ -14,45 +14,45 @@ public class KeinaCyrstalProject {
         System.out.println("Type your comment here: ");
         String userInput = myScanner.nextLine().toLowerCase();
         ArrayList<String> sentences = new ArrayList<String>();
-        sentences.add(userInput.split(". "));
+        //sentences.add(userInput.split(". "));
         int i = 0;
-        String[] word = new String[];
-        while (i < sentences.length){
-            sentence = sentences[i];
+        //String[] word = new String[];
+        //while (i < sentences.length){
+            //sentence = sentences[i];
         }
 
 
         File positive = new File("positive.txt");
-        Scanner po = new Scanner(positive);
+        //Scanner po = new Scanner(positive);
 
         ArrayList<String> pos = new ArrayList<String>();
 
-        while (po.hasNext()) {
-            pos.add(po.nextLine());
+        //while (po.hasNext()) {
+            //pos.add(po.nextLine());
         }
 
-        File negative = new File("negative.txt");
-        Scanner ne = new Scanner(negative);
+        //File negative = new File("negative.txt");
+        //Scanner ne = new Scanner(negative);
 
         ArrayList<String> neg = new ArrayList<String>();
 
-        while (ne.hasNext()) {
-            neg.add(ne.nextLine());
+        //while (ne.hasNext()) {
+            //neg.add(ne.nextLine());
         }
 
         int counterpos = 0;
         int counterneg = 0;
         int i = 0;
 
-        for (word : sentence){
-                if (userInput.indexOf(word) > -1) {
-                    System.out.println("positive");
-                    break;
+        //for (word : sentence){
+                //if (userInput.indexOf(word) > -1) {
+                    //System.out.println("positive");
+                    //break;
                 }
-                else{
-                    counterpos++;
+                //else{
+                    //counterpos++;
                 }
-                i++;
+                //i++;
 
             }
         if (counterpos == pos.size()) {
--- todo.java
+++ todo.java
@@ -1,17 +1,42 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class todo {
 
+    public static void main(String[] args) {
         ArrayList<String> todolist = new ArrayList<String>();
-        Scanner myScanner = new Scanner(System.in);
         todolist();
 
     }
 
     public static void todolist(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Add a new item to the list: yes or no: ");
         String userInput = myScanner.nextLine().toLowerCase();
+        while (userInput.equals("yes")){
+            String yes1 = myScanner.nextLine();
+            todolist.add(yes1);
+            System.out.println("Add a new item to the list: yes or no: ");
+            userInput = myScanner.nextLine().toLowerCase();
+        }
+        System.out.println("Finalized todo list: ");
+        for (String item : todolist){
+            System.out.println(item);
+        }
+
+        }
+        public static void shuffle(){
+            int num = Math.random()*(todolist.size());
+            int num2 = Math.random()*(todolist.size());
+            int i = 0;
+            while (i < 100){
+                todolist.set(num, todolist.get(num2));
+            }
+            System.out.println(todolist);
+
+        }
 
     }
 
-}
+
